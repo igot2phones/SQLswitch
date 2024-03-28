e117436dc4a5ed800c37e7b1c5976b1a32a80aa5
igot2phones <67795195+igot2phones@users.noreply.github.com>
2024-03-28 16:25:00 +0200

    auto ask for admin privileges

--- SQLswitch.bat
+++ SQLswitch.bat
@@ -1,4 +1,21 @@
 @echo off
+
+ call :isAdmin
+
+ if %errorlevel% == 0 (
+    goto :run
+ ) else (
+    echo Requesting administrative privileges...
+    goto :UACPrompt
+ )
+
+ exit /b
+
+ :isAdmin
+    fsutil dirty query %systemdrive% >nul
+ exit /b
+
+ :run
 net session >nul 2>&1
 if %errorlevel% == 0 (
     echo Success: Administrative permissions confirmed.
@@ -15,3 +32,12 @@ if %errorlevel% == 0 (
     net start "MySQL80"
     echo MySQL80 started.
 )
+ exit /b
+
+ :UACPrompt
+   echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+   echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+   "%temp%\getadmin.vbs"
+   del "%temp%\getadmin.vbs"
+  exit /B`
